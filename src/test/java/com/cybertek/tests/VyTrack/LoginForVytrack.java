eaf757964e44c55c45c119f550c0402748f25c26
altayozyavas <altayozyavas@gmail.com>
2020-09-15 23:44:35 -0500

    9/15

--- src/test/java/com/cybertek/tests/VyTrack/LoginForVytrack.java
+++ src/test/java/com/cybertek/tests/VyTrack/LoginForVytrack.java
@@ -4,11 +4,13 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 
 public class LoginForVytrack {
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws InterruptedException  {
 
         WebDriverManager.chromedriver().setup();
 
@@ -18,15 +20,22 @@ public class LoginForVytrack {
 
         driver.findElement(By.id("prependedInput")).sendKeys("user163");
         driver.findElement(By.id("prependedInput2")).sendKeys("UserUser123"+ Keys.ENTER);
+        Thread.sleep(2000);
 
-
-        Thread.sleep(500);
         driver.findElement(By.cssSelector("span[class='title title-level-1']")).click();
-
-        Thread.sleep(500);
-
+        Thread.sleep(2000);
 
         driver.findElement(By.linkText("Vehicles")).click();
+        Thread.sleep(2000);
+
+        WebElement gridSettings = driver.findElement(By.xpath("//div[@class='column-manager dropdown']"));
+        Thread.sleep(3000);
+
+
+        Assert.assertFalse(gridSettings.isDisplayed(),"Shouldnt be displayed");
+
+        gridSettings.click();
+
 
 
 
